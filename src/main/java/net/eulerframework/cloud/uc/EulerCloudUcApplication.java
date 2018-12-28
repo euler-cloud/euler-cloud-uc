946ef49a4296fed354df362070a0f4f346ba4179
cFrost <cFrost@eulerproject.io>
2018-12-29 00:19:57 +0800

    euler-cloud-starter

--- src/main/java/net/eulerframework/cloud/uc/EulerCloudUcApplication.java
+++ src/main/java/net/eulerframework/cloud/uc/EulerCloudUcApplication.java
@@ -1,12 +1,10 @@
 package net.eulerframework.cloud.uc;
 
-import org.eulerframework.cloud.EnableEulerCloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEulerCloud
 @EnableDiscoveryClient
 public class EulerCloudUcApplication {
 
